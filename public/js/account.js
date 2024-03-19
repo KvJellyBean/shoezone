eecd01ddbb76ff9d1eeb6547c838d6c9fd5ef71e
KvJellyBean <kevinnatan91@gmail.com>
2024-03-19 19:37:50 +0700

    fix: add carousel script
    This commit try to manage carousel with the script from bootstrap

--- public/js/account.js
+++ public/js/account.js
@@ -1,49 +1,63 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const loginregis = document.querySelector(".loginregis");
-    const btnpopup = document.querySelector(".popup-login");
-    btnpopup?.addEventListener("click", function () {
-      loginregis?.classList.add("active-popup");
-    });
-  
-    const loginlink = document.querySelector(".login-link");
-    const registerlink = document.querySelector(".register-link");
-    registerlink?.addEventListener("click", function (event) {
-      event.preventDefault();
-      loginregis?.classList.add("active");
-    });
-  
-    loginlink?.addEventListener("click", function (event) {
-      event.preventDefault();
-      loginregis?.classList.remove("active");
-    });
-  
-    const iconclose = document.querySelector(".close");
-    iconclose?.addEventListener("click", function () {
-      loginregis?.classList.remove("active-popup");
-    });
+  const loginregis = document.querySelector(".loginregis");
+  const btnpopup = document.querySelector(".popup-login");
+  btnpopup?.addEventListener("click", function () {
+    loginregis?.classList.add("active-popup");
   });
-  
-  function validateForm() {
-    const password = document.getElementById("password").value;
-    const confirmPassword = document.getElementById("cpassword").value;
-  
-    if (password !== confirmPassword) {
-      alert("Password and confirm password do not match!");
-      return false;
-    }
-    return true;
+
+  const loginlink = document.querySelector(".login-link");
+  const registerlink = document.querySelector(".register-link");
+  registerlink?.addEventListener("click", function (event) {
+    event.preventDefault();
+    loginregis?.classList.add("active");
+  });
+
+  loginlink?.addEventListener("click", function (event) {
+    event.preventDefault();
+    loginregis?.classList.remove("active");
+  });
+
+  const iconclose = document.querySelector(".close");
+  iconclose?.addEventListener("click", function () {
+    loginregis?.classList.remove("active-popup");
+  });
+});
+
+function validateForm() {
+  const password = document.getElementById("password").value;
+  const confirmPassword = document.getElementById("cpassword").value;
+
+  if (password !== confirmPassword) {
+    alert("Password and confirm password do not match!");
+    return false;
   }
-  
-  function togglePassword(field, iconId) {
-    const input = document.getElementById(field);
-    const icon = document.querySelector(`#${iconId}`);
-  
-    if (input.type === "password") {
-      input.type = "text";
-      icon.className = "fa-solid fa-eye";
-    } else {
-      input.type = "password";
-      icon.className = "fa-solid fa-eye-slash";
-    }
+  return true;
+}
+
+function togglePassword(field, iconId) {
+  const input = document.getElementById(field);
+  const icon = document.querySelector(`#${iconId}`);
+
+  if (input.type === "password") {
+    input.type = "text";
+    icon.className = "fa-solid fa-eye";
+  } else {
+    input.type = "password";
+    icon.className = "fa-solid fa-eye-slash";
   }
-  
+}
+
+// Carousel
+document.addEventListener("DOMContentLoaded", function () {
+  const myCarousel = new bootstrap.Carousel(
+    document.getElementById("carouselExampleAutoplaying"),
+    {
+      interval: 3000,
+      pause: false,
+      wrap: true,
+      keyboard: true,
+      touch: true,
+      slide: true,
+    }
+  );
+});
