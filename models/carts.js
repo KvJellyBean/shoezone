a7711a8968ed1c6b748617d2a8927c5f126620c6
KvJellyBean <kevinnatan91@gmail.com>
2024-04-24 19:34:37 +0700

    fix: refactor cart schema

--- models/carts.js
+++ models/carts.js
@@ -1,12 +1,40 @@
 const { Schema, model } = require("mongoose");
 
-const cartSchema = new Schema({
-  name: String,
-  price: Number,
-  description: String,
-  image: String,
-  quantity: { type: Number, default: 1 },
-  totalPrice: Number,
-});
+const cartSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "Account",
+    },
+    products: [
+      {
+        name: {
+          type: String,
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+          min: 0,
+        },
+        description: String,
+        image: String,
+        rating: {
+          type: Number,
+          min: 0,
+          max: 5,
+          default: 0,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+          default: 1,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 module.exports = model("Cart", cartSchema);
