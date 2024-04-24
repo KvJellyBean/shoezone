6fca9231dab9b702c8fd3ea20af59031d0c6c3f5
KvJellyBean <kevinnatan91@gmail.com>
2024-04-24 19:45:25 +0700

    feat: add and adjust some CRUD methods for the cart

--- routes/api/carts.js
+++ routes/api/carts.js
@@ -13,21 +13,24 @@ router.get("/", async (req, res) => {
   }
 });
 
+// Method get by userId
+router.get("/:userId", async (req, res) => {
+  try {
+    const items = await Carts.find({ userId: req.params.userId });
+    res.status(200).json(items);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 // Method Post
 router.post("/", async (req, res) => {
   try {
-    const newProducts = new Carts({
-      ...req.body,
-      _id: new mongoose.Types.ObjectId(), // Generate a new ObjectId
-    });
-    const savedProducts = await newProducts.save();
-    if (!savedProducts) {
-      res.status(500).json({ message: "Internal Server Error" });
-    }
-
-    res.status(200).json(savedProducts);
+    const newCart = new Carts(req.body);
+    await newCart.save();
+    res.status(201).json(newCart);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(400).json({ message: error.message });
   }
 });
 
@@ -49,18 +52,57 @@ router.put("/:id", async (req, res) => {
   }
 });
 
-// Method Delete
-router.delete("/:id", async (req, res) => {
+// Method put untuk mengubah/mengupdate quantity product berdasarkan quantity di req body ke cart berdasarkan userId yang didalamnya terdapat key products yang berisi array of object tiap productnya
+router.put("/:userId/:productId", async (req, res) => {
   try {
-    const product = await Carts.findById(req.params.id);
-    if (product) {
-      await Carts.findByIdAndDelete(req.params.id);
-      res.status(200).json("Product deleted successfully");
-    } else {
-      res.status(404).json({ message: "Product not found" });
+    const updatedCart = await Carts.findOneAndUpdate(
+      { userId: req.params.userId, "products._id": req.params.productId },
+      { $set: { "products.$.quantity": req.body.quantity } },
+      { new: true }
+    );
+    if (!updatedCart) {
+      res.status(404).json({ message: "Not Found" });
     }
+
+    res.status(200).json(updatedCart);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
+
+// Method Delete untuk mengosongkan array products dari user cart by userId, bukan menghapus cart nya
+router.delete("/:userId", async (req, res) => {
+  try {
+    const updatedCart = await Carts.findOneAndUpdate(
+      { userId: req.params.userId },
+      { products: [] },
+      { new: true }
+    );
+    if (!updatedCart) {
+      res.status(404).json({ message: "Not Found" });
+    }
+
+    res.status(200).json(updatedCart);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.delete("/:userId/:productId", async (req, res) => {
+  try {
+    const updatedCart = await Carts.findOneAndUpdate(
+      { userId: req.params.userId },
+      { $pull: { products: { _id: req.params.productId } } },
+      { new: true }
+    );
+    if (!updatedCart) {
+      res.status(404).json({ message: "Not Found" });
+    }
+
+    res.status(200).json(updatedCart);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 module.exports = router;
